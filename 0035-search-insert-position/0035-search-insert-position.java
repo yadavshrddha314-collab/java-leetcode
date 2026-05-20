fc7e68eba84d526c8951b43f33bb36472b3a50f9
yadavshrddha314-collab <yadavshrddha314@gmail.com>
2026-05-20 20:05:02 +0530

    Time: 0 ms (100%), Space: 44.8 MB (47.08%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int n= nums.length;
+        int low=0;
+        int high= n-1;
+        while(low <= high ){
+            int mid =(low + high)/2;
+            if(nums[mid] == target) return mid;
+            else if (nums[mid]> target)high=mid-1;
+            else low=mid+1;
+        }
+        return low; //why because the intesetion of low and high will aways have//
+    }
+}
